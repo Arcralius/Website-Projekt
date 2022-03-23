a5521502ce0fbd350bff74667681dbe3c1a6a4e4
Arcralius <33646007+Arcralius@users.noreply.github.com>
2022-03-23 22:01:19 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,13 +1,10 @@
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "password";
-	$dbname = "ict1004_assignment";
-
+$config = parse_ini_file("../../private/db-config.ini");
 	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
+	$config = parse_ini_file("../../private/db-config.ini");
+	$conn = new mysqli($config["servername"], $config["username"],
+		$config["password"], $config["dbname"]);
 	// Check connection
 	if ($conn->connect_error) {
 	  die("Connection failed: " . $conn->connect_error);
 	}
-?>
