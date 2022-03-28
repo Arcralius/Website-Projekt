2d9eccb9b611fbe8a5344e91a650f12f4b5d7750
Arcralius <33646007+Arcralius@users.noreply.github.com>
2022-03-28 17:57:43 +0800

    fixed product bugs

--- product.php
+++ product.php
@@ -52,11 +52,6 @@
             <p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p>
         </div>
     </footer>
-    <!-- Bootstrap core JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Core theme JS-->
-    <script src="js/scripts.js"></script>
-
     <?php
     function getproducts()
     {
