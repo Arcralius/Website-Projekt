d9e804fdd4a100d3978e44d377f84405f772a216
Arcralius <33646007+Arcralius@users.noreply.github.com>
2022-03-31 18:16:40 +0800

    footer

--- product.php
+++ product.php
@@ -53,13 +53,11 @@
     </section>
 
 
-    <!-- Footer-->
-    <footer class="py-5 bg-dark">
-        <div class="container">
-            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p>
-        </div>
-    </footer>
+
+
     <?php
+
+    include 'footer.php';
     function getproducts()
     {
         require("conn.php");
