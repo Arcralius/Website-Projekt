a18348494b4b2d3e94f7d80c30d91c5543135181
Arcralius <33646007+Arcralius@users.noreply.github.com>
2022-04-06 19:27:49 +0800

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+
+<head>
+  <?php
+  include 'header.php';
+  ?>
+</head>
+
+<body>
+  <main>
+    <?php
+    include 'navbar.php';
+    ?>
+    <!-- Header-->
+    <header class="bg-dark py-5">
+      <div class="container px-4 px-lg-5 my-5">
+        <div class="text-center text-white">
+          <h1 class="display-4 fw-bolder">404 Not Found</h1>
+        </div>
+      </div>
+    </header>
+    <!-- Section-->
+
+    <?php
+    include 'footer.php';
+    ?>
+
+
+  </main>
+</body>
+
+</html>
