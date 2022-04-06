7074fff99e86eac47ebe85424837495a44258ea3
Arcralius <33646007+Arcralius@users.noreply.github.com>
2022-04-06 18:09:21 +0800

    Update product.php

--- product.php
+++ product.php
@@ -2,13 +2,6 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-    <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
     <?php
     include 'header.php';
@@ -206,8 +199,7 @@
             return $discount2;
         }
         ?>
-</main>
+    </main>
 </body>
-
 
 </html>
